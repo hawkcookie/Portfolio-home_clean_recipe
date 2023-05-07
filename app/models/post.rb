4fa6506c9088596fec439726606263dc0f43b069
hawkcookie <qian41119122@gmail.com>
2023-05-07 17:15:35 +0900

    ストレージ参照削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,11 +18,8 @@ class Post < ApplicationRecord
   #validate :validates_cleaning_recipes
   
   def get_post_image(width,height)
-    unless post_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       post_image.attach(io:File.open(file_path),filename: 'no_image.jpg',content_type: 'image/jpeg')
-    end
-    post_image.variant(resize_to_limit:[width,height]).processed
   end
   
   def liked_by?(user)
